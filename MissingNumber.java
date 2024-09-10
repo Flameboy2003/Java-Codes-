6cf4e40c65e4a1b1ac06b934458273ee9ea5f0d1
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-09-10 20:48:54 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,12 +1,26 @@
+//Question : 268
+
 import java.util.Scanner;
 
 public class MissingNumber {
     /*
-    Input: nums = [9,6,4,2,3,5,7,0,1]
-    Output: 8
-    Explanation: n = 9 since there are 9 numbers,
-    so all numbers are in the range [0,9].
-    8 is the missing number in the range since it does not appear in nums.
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+Example 2:
+Input: nums = [0,1]
+Output: 2
+
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
      */
     public int findMissingNumber(int[] nums) {
         int n = nums.length;
@@ -28,8 +42,6 @@ public class MissingNumber {
     }
 
     public static void main(String[] args) {
-
-        //All the numbers of nums are unique.
 
         int[] arr = {0, 1, 2, 3, 4};
         MissingNumber obj = new MissingNumber();
